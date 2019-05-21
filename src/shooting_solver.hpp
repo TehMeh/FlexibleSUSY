047fc18dc71e55df76cc5637c97937910fc66c5c
Alexander Voigt <ahgvoigt@gmail.com>
2019-05-21 12:33:14 +0200

    first implementation of generic shooting solver

--- src/shooting_solver.hpp
+++ src/shooting_solver.hpp
@@ -20,6 +20,10 @@
 #define SHOOTING_SOLVER_H
 
 #include "composite_root_finder.hpp"
+#include "logger.hpp"
+#include "model.hpp"
+#include "single_scale_constraint.hpp"
+#include "single_scale_matching.hpp"
 #include <cstddef>
 #include <vector>
 #include <Eigen/Core>
@@ -28,14 +32,24 @@ namespace flexiblesusy {
 
 /**
  * @class Shooting_solver
- * @brief Solves the BVP using the shooting method
+ * @brief Solves the IVP using the shooting method
  */
 template<std::size_t N>
 class Shooting_solver {
 public:
    using Vec_t = Eigen::Matrix<double, N, 1>;
    using Fun_t = std::function<Vec_t(const Vec_t&)>;
+   using Pre_t = std::function<Vec_t()>;
+   using Set_t = std::function<void(const Vec_t&)>;
 
+   /// add constraint
+   void add(Single_scale_constraint*, Model*);
+   /// add matching condition
+   void add(Single_scale_matching*, Model*, Model*);
+   /// add predictor
+   void add(const Pre_t& p) { predictor = p; }
+   /// add parameter setter
+   void add(const Set_t& s) { setter = s; }
    /// returns solution
    Vec_t get_solution() const { return solution; }
    /// set precision goal
@@ -45,23 +59,141 @@ public:
    /// clear all internal data
    void reset();
    /// solves the boundary value problem with initial guess
-   void solve(const Fun_t&, const Vec_t&);
+   void solve(const Vec_t&);
 
 private:
-   Vec_t solution{Vec_t::Zero()};
-   double precision{1e-3};
-   std::size_t max_it{100};
+   struct Slider {
+   public:
+      virtual ~Slider() {}
+      virtual void clear_problems() {}
+      virtual double get_scale() = 0;
+      virtual void slide() {}
+   };
+
+   struct Constraint_slider : public Slider {
+   public:
+      Constraint_slider(Model* m, Single_scale_constraint* c)
+         : model(m), constraint(c) {}
+      virtual ~Constraint_slider() = default;
+      virtual void clear_problems() override { model->clear_problems(); }
+      virtual double get_scale() override { return constraint->get_scale(); }
+      virtual void slide() override {
+         const auto Q = get_scale();
+         VERBOSE_MSG("> \trunning " << model->name() << " to scale " << Q << " GeV");
+         model->run_to(Q);
+         VERBOSE_MSG("> \tapplying " << constraint->name());
+         constraint->apply();
+      }
+   private:
+      Model* model;
+      Single_scale_constraint* constraint;
+   };
+
+   struct Matching_slider : public Slider {
+   public:
+      Matching_slider(Model* m1_, Model* m2_, Single_scale_matching* mc)
+         : m1(m1_), m2(m2_), matching(mc) {}
+      virtual ~Matching_slider() = default;
+      virtual void clear_problems() override {
+         m1->clear_problems();
+         m2->clear_problems();
+      }
+      virtual double get_scale() override { return matching->get_scale(); }
+      virtual void slide() override {
+         const auto Q = get_scale();
+         VERBOSE_MSG("> \trunning " << m1->name() << " to scale " << Q << " GeV");
+         m1->run_to(Q);
+         VERBOSE_MSG("> \trunning " << m2->name() << " to scale " << Q << " GeV");
+         m2->run_to(Q);
+         VERBOSE_MSG("> \tmatching " << m1->name() << " -> " << m2->name());
+         matching->match();
+      }
+   private:
+      Model *m1, *m2;
+      Single_scale_matching* matching;
+   };
+
+   std::vector<std::shared_ptr<Slider> > sliders{}; ///< sliders to be run
+   Vec_t solution{Vec_t::Zero()}; ///< stored solution
+   double precision{1e-3};        ///< running precision
+   std::size_t max_it{100};       ///< maximum number of iterations
+   Pre_t predictor{nullptr};      ///< calculates prediction
+   Set_t setter{nullptr};         ///< sets parameters
 };
 
+/**
+ * Adding a model constraint
+ *
+ * @param c constraint
+ * @param m model
+ */
+template<std::size_t N>
+void Shooting_solver<N>::add(Single_scale_constraint* c, Model* m)
+{
+   if (!c) throw SetupError("constraint pointer is NULL");
+   if (!m) throw SetupError("model pointer is NULL");
+   sliders.push_back(std::make_shared<Constraint_slider>(m, c));
+}
+
+/**
+ * Adds a matching condition.  This matching condition matches the two
+ * models by calling match().  The two models are passed to the
+ * set_models() function of the matching condition.
+ *
+ * @param mc matching condition
+ * @param m1 model 1
+ * @param m2 model 2
+ */
+template<std::size_t N>
+void Shooting_solver<N>::add(Single_scale_matching* mc, Model* m1, Model* m2)
+{
+   if (!mc) throw SetupError("matching condition pointer is NULL");
+   if (!m1) throw SetupError("model pointer 1 is NULL");
+   if (!m2) throw SetupError("model pointer 2 is NULL");
+   mc->set_models(m1, m2);
+   sliders.push_back(std::make_shared<Matching_slider>(m1, m2, mc));
+}
+
+/**
+ * Reset solver
+ */
 template<std::size_t N>
 void Shooting_solver<N>::reset()
 {
-   *this = Shooting_solver<N>();
+   sliders.clear();
+   solution = Vec_t::Zero();
+   precision = 1e-3;
+   max_it = 100;
+   predictor = nullptr;
+   setter = nullptr;
 }
 
+/**
+ * Solves the IVP by finding the root of the function defined by the
+ * following procdure:
+ *
+ * 1. setting the parameters (using the setter)
+ * 2. imposing all boundary and matching conditions
+ * 3. calculating the output (calling the predictor)
+ *
+ * @param v0 initial parameter guess
+ */
 template<std::size_t N>
-void Shooting_solver<N>::solve(const Fun_t& fun, const Vec_t& v0)
+void Shooting_solver<N>::solve(const Vec_t& v0)
 {
+   if (!predictor)
+      throw SetupError("No predictor set!");
+   if (!setter)
+      throw SetupError("No setter set!");
+
+   Fun_t fun = [&] (const Vec_t& v) {
+      setter(v);
+      for (auto& s: sliders) {
+         s->slide();
+      }
+      return predictor();
+   };
+
    Composite_root_solver<N> crs;
    crs.set_precision(precision);
    crs.set_max_iterations(max_it);
--- test/test_shooting_solver.cpp
+++ test/test_shooting_solver.cpp
@@ -7,30 +7,53 @@
 #include "shooting_solver.hpp"
 #include "wrappers.hpp"
 
+#include "mock_models.hpp"
+#include "mock_single_scale_constraints.hpp"
+#include "mock_single_scale_matchings.hpp"
+
 using namespace flexiblesusy;
 
-typedef Eigen::Matrix<double,2,1> EV2_t;
-
-BOOST_AUTO_TEST_CASE( test_parabola_2dim )
+BOOST_AUTO_TEST_CASE( test_trival_matching )
 {
-   auto parabola = [](const EV2_t& x) -> EV2_t {
-      const double y = x(0);
-      const double z = x(1);
-      EV2_t f;
-      f << y*(y - 5.0), z*(z - 1.0);
-      return f;
+   static const int N = 10;
+   using Vec_t = Shooting_solver<N>::Vec_t;
+   using Pre_t = Shooting_solver<N>::Pre_t;
+   using Set_t = Shooting_solver<N>::Set_t;
+
+   // parameters to be predicted
+   Vec_t prediction_goal(Vec_t::Zero());
+   for (int i = 0; i < N; ++i)
+      prediction_goal(i) = i + 1;
+
+   Static_model eft(Vec_t::Zero()), bsm(Vec_t::Zero());
+   Counting_constraint ceft(100), cbsm(1000);
+
+   // this trivial matching condition simply forwards the parameters
+   // of one model to the other
+   Trivial_matching_condition mc;
+
+   Pre_t pred = [&] () {
+      return Vec_t(prediction_goal - eft.get_parameters());
    };
 
-   Eigen::Matrix<double,2,1> start;
-   start << 10, 10;
+   Set_t sett = [&] (const Vec_t& v) {
+      bsm.set_parameters(v);
+   };
 
-   Shooting_solver<2> ss;
-   ss.set_precision(1e-5);
+   Shooting_solver<N> solver;
+   solver.add(&cbsm, &bsm);
+   solver.add(&mc, &bsm, &eft);
+   solver.add(&ceft, &eft);
+   solver.add(pred);
+   solver.add(sett);
 
-   BOOST_REQUIRE_NO_THROW(ss.solve(parabola, start));
+   const Vec_t start(Vec_t::Zero());
 
-   const auto sol = ss.get_solution();
+   BOOST_CHECK_NO_THROW(solver.solve(start));
 
-   BOOST_CHECK_CLOSE_FRACTION(sol(0), 5.0, 1.0e-5);
-   BOOST_CHECK_CLOSE_FRACTION(sol(1), 1.0, 1.0e-5);
+   // the high scale parameters should be the same as the low scale
+   // parameters
+   BOOST_CHECK_LT((eft.get_parameters() - prediction_goal).cwiseAbs().sum(), 1e-10);
+   BOOST_CHECK_LT((bsm.get_parameters() - prediction_goal).cwiseAbs().sum(), 1e-10);
 }
+
